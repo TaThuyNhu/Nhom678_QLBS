35b61d39d4b513b8b066e847972923e64cc977d1
hoang18tuoi <110008317+hoang18tuoi@users.noreply.github.com>
2022-11-15 23:08:54 +0700

    Thêm Kho

---
+++ Nhom678_DuAn1_QLBanSach/1.DAL/IRepositories/IKhoRepository.cs
@@ -0,0 +1,19 @@
+﻿using _1.DAL.DomainClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAL.IRepositories
+{
+    public interface IKhoRepository
+    {
+        bool AddKho(Kho k);
+        bool UpdateKho(Kho k);
+        bool DeleteKho(Kho k);
+        Kho GetById(Guid id);
+        List<Kho> GetAllKho();
+
+    }
+}
