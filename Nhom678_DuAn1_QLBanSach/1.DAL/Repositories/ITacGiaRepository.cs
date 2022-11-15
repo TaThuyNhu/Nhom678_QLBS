a959afab0429a06df14ebf73afd046f4f1d7ca27
hoang18tuoi <110008317+hoang18tuoi@users.noreply.github.com>
2022-11-15 23:57:06 +0700

    Xong tầng 1

---
+++ Nhom678_DuAn1_QLBanSach/1.DAL/Repositories/ITacGiaRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace _1.DAL.Repositories
+{
+    internal interface ITacGiaRepository
+    {
+    }
+}
---
+++ Nhom678_DuAn1_QLBanSach/1.DAL/Repositories/TaiKhoanRepository.cs
@@ -0,0 +1,58 @@
+﻿using _1.DAL.Context;
+using _1.DAL.DomainClass;
+using _1.DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAL.Repositories
+{
+    public class TaiKhoanRepository : ITaiKhoanRepository
+    {
+        private Nhom678 _dBContext;
+        List<TaiKhoan> _TaiKhoanList;
+        public TaiKhoanRepository()
+        {
+            _dBContext = new Nhom678();
+            _TaiKhoanList = new List<TaiKhoan>();
+        }
+        public bool AddTaiKhoan(TaiKhoan tk)
+        {
+            if (tk == null) return false;
+            _dBContext.TaiKhoans.Add(tk);
+            _dBContext.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteTaiKhoan(TaiKhoan tk)
+        {
+            if (tk == null) return false;
+            var temp = _dBContext.TaiKhoans.FirstOrDefault(c => c.MaTaiKhoan == tk.MaTaiKhoan);
+            _dBContext.TaiKhoans.Remove(temp);
+            _dBContext.SaveChanges();
+            return true;
+        }
+
+        public List<TaiKhoan> GetAllTaiKhoan()
+        {
+            _TaiKhoanList = _dBContext.TaiKhoans.ToList();
+            return _TaiKhoanList;
+        }
+
+        public TaiKhoan GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTaiKhoan(TaiKhoan tk)
+        {
+            if (tk == null) return false;
+            var temp = _dBContext.TaiKhoans.FirstOrDefault(c => c.MaTaiKhoan == tk.MaTaiKhoan);
+            _dBContext.TaiKhoans.Update(temp);
+            _dBContext.SaveChanges();
+            return true;
+        }
+    }
+}
