b23e07e5b0d35626bedbc75f3b9aefa3acd3955b
khanhlvph26150 <khanhlvph26150@fpt.edu.vn>
2022-11-16 07:39:04 +0700

    Vẫn còn bảng QLTaiKhoanService

---
+++ Nhom678_DuAn1_QLBanSach/2.BUS/Services/QLLoaiHinhGDService.cs
@@ -0,0 +1,80 @@
+﻿using _1.DAL.DomainClass;
+using _1.DAL.IRepositories;
+using _1.DAL.Repositories;
+using _2.BUS.IServices;
+using _2.BUS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.BUS.Services
+{
+    public class QLLoaiHinhGDService
+    {
+        private ILoaiHinhGiaoDichRepository _loaiHinhGiaoDichRepository;
+        public QLLoaiHinhGDService()
+        {
+            _loaiHinhGiaoDichRepository = new LoaiHinhGiaoDichRepository();
+        }
+        public string Add(LoaiHinhGiaoDichView LoaiHinhGiaoDich)
+        {
+            if (LoaiHinhGiaoDich == null) return "Them LoaiHinhGiaoDichng thanh cong";
+            var LoaiHinhGiaoDichhang = new LoaiHinhGiaoDich()
+            {
+                MaLoaiGiaoDich = LoaiHinhGiaoDich.MaLoaiGiaoDich,
+                MaKhachHang = LoaiHinhGiaoDich.MaKhachHang,
+                LoaiHinh1 = LoaiHinhGiaoDich.LoaiHinh1,
+                LoaiHinh2 = LoaiHinhGiaoDich.LoaiHinh2
+            };
+            if (_loaiHinhGiaoDichRepository.AddLoaiHinhGiaoDich(LoaiHinhGiaoDichhang))
+                return "Them thanh cong";
+            return "Them LoaiHinhGiaoDichng thanh cong";
+        }
+
+        public string Delete(LoaiHinhGiaoDichView LoaiHinhGiaoDich)
+        {
+            if (LoaiHinhGiaoDich == null) return "xoa LoaiHinhGiaoDichng thanh cong";
+            var LoaiHinhGiaoDichhang = new LoaiHinhGiaoDich()
+            {
+                MaLoaiGiaoDich = LoaiHinhGiaoDich.MaLoaiGiaoDich,
+                MaKhachHang = LoaiHinhGiaoDich.MaKhachHang,
+                LoaiHinh1 = LoaiHinhGiaoDich.LoaiHinh1,
+                LoaiHinh2 = LoaiHinhGiaoDich.LoaiHinh2
+            };
+            if (_loaiHinhGiaoDichRepository.AddLoaiHinhGiaoDich(LoaiHinhGiaoDichhang))
+                return "xoa thanh cong";
+            return "xoa LoaiHinhGiaoDichng thanh cong";
+        }
+
+        public List<LoaiHinhGiaoDichView> GetAll()
+        {
+            List<LoaiHinhGiaoDichView> list = new List<LoaiHinhGiaoDichView>();
+            list = (from n in _loaiHinhGiaoDichRepository.GetAllLoaiHinhGiaoDich()
+                    select new LoaiHinhGiaoDichView
+                    {
+                       MaLoaiGiaoDich = n.MaLoaiGiaoDich,
+                       MaKhachHang = n.MaKhachHang,
+                       LoaiHinh1 = n.LoaiHinh1,
+                       LoaiHinh2 = n.LoaiHinh2
+                    }).ToList();
+            return list;
+        }
+
+        public string Update(LoaiHinhGiaoDichView LoaiHinhGiaoDich)
+        {
+            if (LoaiHinhGiaoDich == null) return "sua LoaiHinhGiaoDichng thanh cong";
+            var LoaiHinhGiaoDichhang = new LoaiHinhGiaoDich()
+            {
+                MaLoaiGiaoDich = LoaiHinhGiaoDich.MaLoaiGiaoDich,
+                MaKhachHang = LoaiHinhGiaoDich.MaKhachHang,
+                LoaiHinh1 = LoaiHinhGiaoDich.LoaiHinh1,
+                LoaiHinh2 = LoaiHinhGiaoDich.LoaiHinh2
+            };
+            if (_loaiHinhGiaoDichRepository.UpdateLoaiHinhGiaoDich(LoaiHinhGiaoDichhang))
+                return "sua thanh cong";
+            return "sua LoaiHinhGiaoDichng thanh cong";
+        }
+    }
+}
---
+++ Nhom678_DuAn1_QLBanSach/2.BUS/Services/QLNhaXuatBanService.cs
@@ -0,0 +1,79 @@
+﻿using _1.DAL.DomainClass;
+using _1.DAL.IRepositories;
+using _1.DAL.Repositories;
+using _2.BUS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.BUS.Services
+{
+    public class QLNhaXuatBanService
+    {
+        private INhaXuatBanRepository _nhaXuatBanRepository;
+        public QLNhaXuatBanService()
+        {
+            _nhaXuatBanRepository = new NhaXuatBanRepository();
+        }
+        public string Add(NxbView nxb)
+        {
+            if (nxb == null) return "Them khong thanh cong";
+            var khohang = new NhaXuatBan()
+            {
+                MaNxb = nxb.MaNxb,
+                TenNxb = nxb.TenNxb,
+                DiaChi = nxb.DiaChi,
+                Url = nxb.Url,
+            };
+            if (_nhaXuatBanRepository.AddNhaXuatBan(khohang))
+                return "Them thanh cong";
+            return "Them khong thanh cong";
+        }
+
+        public string Delete(NxbView nxb)
+        {
+            if (nxb == null) return "xoa khong thanh cong";
+            var nhaXuatBan = new NhaXuatBan()
+            {
+                MaNxb = nxb.MaNxb,
+                TenNxb = nxb.TenNxb,
+                DiaChi = nxb.DiaChi,
+                Url = nxb.Url,
+            };
+            if (_nhaXuatBanRepository.DeleteNhaXuatBan(nhaXuatBan))
+                return "xoa thanh cong";
+            return "xoa khong thanh cong";
+        }
+
+        public List<NxbView> GetAll()
+        {
+            List<NxbView> list = new List<NxbView>();
+            list = (from n in _nhaXuatBanRepository.GetAllNhaXuatBan()
+                    select new NxbView
+                    {
+                        MaNxb = n.MaNxb,
+                        TenNxb= n.TenNxb,
+                        DiaChi= n.DiaChi,
+                        Url = n.Url,
+                    }).ToList();
+            return list;
+        }
+
+        public string Update(NxbView nxb)
+        {
+            if (nxb == null) return "sua khong thanh cong";
+            var nhaXuatBan = new NhaXuatBan()
+            {
+                MaNxb = nxb.MaNxb,
+                TenNxb = nxb.TenNxb,
+                DiaChi = nxb.DiaChi,
+                Url = nxb.Url,
+            };
+            if (_nhaXuatBanRepository.UpdateNhaXuatBan(nhaXuatBan))
+                return "sua thanh cong";
+            return "sua khong thanh cong";
+        }
+    }
+}
